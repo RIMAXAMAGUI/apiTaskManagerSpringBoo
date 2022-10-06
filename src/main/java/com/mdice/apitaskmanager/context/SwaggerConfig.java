d6a7d2f534b568602fe96bafe8862062889d884d
MartinDiCe <jamartindice@gmail.com>
2022-10-06 08:48:06 -0300

    Subiendo un cambio

--- src/main/java/com/mdice/apitaskmanager/context/SwaggerConfig.java
+++ src/main/java/com/mdice/apitaskmanager/context/SwaggerConfig.java
@@ -1,2 +1,37 @@
-package com.mdice.apitaskmanager.context;public class SwaggerConfig {
+package com.mdice.apitaskmanager.context;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+import java.util.Collections;
+
+@Configuration
+
+public class SwaggerConfig {
+    @Bean
+    public Docket api() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .select()
+                .apis(RequestHandlerSelectors.basePackage("com.mdice.apiTaskManager.controller"))
+                .paths(PathSelectors.any())
+                .build()
+                .apiInfo(apiInfo());
+    }
+
+    private ApiInfo apiInfo() {
+        return new ApiInfo(
+                "Api Task Manager",
+                "My first api rest springboot",
+                "v0.1",
+                "Term of service",
+                new Contact("Mdice Tech","https://github.com/MartinDiCe/apiTaskManagerSpringBoot/tree/dev", "jamartindice@gmail.com"),
+                "License of API", "Api License URL", Collections.emptyList());
+    }
 }
--- src/main/java/com/mdice/apitaskmanager/controller/TaskController.java
+++ src/main/java/com/mdice/apitaskmanager/controller/TaskController.java
@@ -1,2 +1,12 @@
-package com.mdice.apitaskmanager.controller;public class TaskController {
+package com.mdice.apitaskmanager.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TaskController {
+    @RequestMapping("/")
+    public void handleRequest() {
+        throw new RuntimeException("test exception");
+    }
 }
