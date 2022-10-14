681ffdabf5f5ba77e2b48ae416b423a8f1623fdd
MartinDiCe <jamartindice@gmail.com>
2022-10-14 10:36:05 -0300

    Subiendo el getTasks

--- src/main/java/com/mdice/apitaskmanager/persistence/repositoy/TaskRepository.java
+++ src/main/java/com/mdice/apitaskmanager/persistence/repositoy/TaskRepository.java
@@ -2,7 +2,7 @@ package com.mdice.apitaskmanager.persistence.repositoy;
 
 import com.mdice.apitaskmanager.persistence.entity.Task;
 import org.springframework.data.jpa.repository.JpaRepository;
-
+//Jpa tiene un metodo para crear y para obtener
 public interface TaskRepository extends JpaRepository <Task, Long> {
 
 }
--- src/main/java/com/mdice/apitaskmanager/service/TaskService.java
+++ src/main/java/com/mdice/apitaskmanager/service/TaskService.java
@@ -6,6 +6,8 @@ import com.mdice.apitaskmanager.persistence.repositoy.TaskRepository;
 import com.mdice.apitaskmanager.service.dto.TaskInDTO;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
 public class TaskService {
 
@@ -20,5 +22,8 @@ public class TaskService {
     public Task createTask(TaskInDTO taskInDTO){
         Task task  = mapper.map(taskInDTO);
         return this.repository.save(task);
+    }
+    public List<Task> findAll(){
+        return this.repository.findAll();
     }
 }
