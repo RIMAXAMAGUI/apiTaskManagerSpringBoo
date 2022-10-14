d3d1421228279dac2ed75384814b7d9009361a21
MartinDiCe <jamartindice@gmail.com>
2022-10-14 10:36:05 -0300

    Subiendo el getTasks

--- src/main/java/com/mdice/apitaskmanager/controller/TaskController.java
+++ src/main/java/com/mdice/apitaskmanager/controller/TaskController.java
@@ -3,10 +3,9 @@ package com.mdice.apitaskmanager.controller;
 import com.mdice.apitaskmanager.persistence.entity.Task;
 import com.mdice.apitaskmanager.service.TaskService;
 import com.mdice.apitaskmanager.service.dto.TaskInDTO;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/tasks")
@@ -18,6 +17,10 @@ public class TaskController {
     @PostMapping
     public Task createTask(@RequestBody TaskInDTO taskInDTO ){
         return this.taskService.createTask(taskInDTO);
-
     }
+    @GetMapping
+    public List<Task> findAll(){
+        return this.taskService.findAll();
+    }
+
 }
